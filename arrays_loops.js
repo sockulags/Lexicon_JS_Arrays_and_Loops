e1efb8a17fcb487bc491c31ca578337bd8904878
Lucas Skog <lucasskog@gmail.com>
2024-01-10 17:41:46 +0100

    first three done

---
+++ arrays_loops.js
@@ -0,0 +1,40 @@
+//Rules: 
+//When your are completing these exercises, you are free do use any loop you want: while, for, foreach or do-while. Use the one that best suits you best. foreach and do-while is two loops that we haven't covered when creating this exercise. Google them if you want to.
+
+// Create an empty array and fill it with different elements using the push-method. Log it to the console.
+const arr1 = [];
+arr1.push(true)
+arr1.push(30);
+arr1.push({elephant: "heavy", color: "pink"})
+console.log(arr1)
+
+// Create an array containing arbitrary numbers. Loop throught that array and multiply each element with 3 and log that to the console.
+const arr2 = [];
+let iterations = Math.random() * 20;
+for (let x = 0; x < iterations; x++){
+    let nr = Math.ceil(Math.random()*20);  
+    arr2.push(nr)
+} 
+console.log(arr2)
+for(let x= 0; x < arr2.length; x++){
+    arr2[x] *= 3;
+}
+console.log(arr2)
+
+// Create an array with numbers, and one empty array. Loop through the array with numbers and do an if check in each iteration. If that number is bigger then a specific number, 5 for example, add that number to the empty array. Log the new array to the console in the end. Remember to use the indexes to get the elements from the array with numbers.
+const arr3= [4,22,1,4,7,9,42];
+const mtArr = [];
+iterations = 0
+while(iterations < arr3.length){
+    if(arr3[iterations] < 7){
+        mtArr.push(arr3[iterations])
+    }
+    iterations++;
+}
+console.log(mtArr)
+// Create an array with random numbers. Loop through the array and add them all together. Log the total sum of all the elements to the console. hint - You will need a variable here that store the sum as you iterate over the array.
+// Create an array with some elements. Now search for a specific element while you are looping through the array. If you find the element you are searching for, log it to the console.
+// Create an array with some elements, remove the last element WITHOUT using pop().
+// Create an array with 10 different numbers. Find the biggest number in the array and log it to the console. hint - You will need a variable that holds the current biggest number.
+// Create an array with some elements and an empty array. Using a loop, copy the content of the first array to the empty array.
+// Create an array with some elements. Use a loop to empty the entire array so there are no elements left in the end.
